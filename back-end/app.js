fa57abadfda2678fb5950f36dfa5c296f1892236
Marvinmartain <122340360+Marvinmartain@users.noreply.github.com>
2023-04-25 15:06:05 -0400

    again

--- back-end/app.js
+++ back-end/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-app.use("/api", (req, res, next) => {
-        res.send("Hello World")
-})
+mongoose.connect("mongodb+srv://aareeuh:gOUNtmhcOcTXaPtl@cluster0.8zciamf.mongodb.net/Blog?retryWrites=true&w=majority")
+  .then(() => app.listen(5000))
+  .then(() => console.log("Connected To Database and Listening TO Localhost 5000"))
+  .catch((err) => console.log(err));
 
-app.listen(5000);
+
+
